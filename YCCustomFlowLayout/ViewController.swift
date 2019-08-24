5af25356d7e857c354550a94e14ffc1ea4fef012
cinaryusufiu <cinaryusufiu@gmail.com>
2019-08-25 02:59:00 +0300

    Initial Commit

---
+++ YCCustomFlowLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YCCustomFlowLayout
+//
+//  Created by Yusuf Çınar on 25.08.2019.
+//  Copyright © 2019 Yusuf Cinar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
